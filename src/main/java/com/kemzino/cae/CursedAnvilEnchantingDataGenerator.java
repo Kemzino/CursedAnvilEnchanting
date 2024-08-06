b7da7aba29c911c7f47ff0342fbd0e1b56dbca46
IhorDovh <holdargraf@gmail.com>
2024-08-06 20:35:48 +0300

    first commit

---
+++ src/main/java/com/kemzino/cae/CursedAnvilEnchantingDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kemzino.cae;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CursedAnvilEnchantingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
